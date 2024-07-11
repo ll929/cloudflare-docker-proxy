0db40514773352186459586b85ba4e5089ad0a52
LiuLei <liuleiytu@gmail.com>
2024-07-11 11:42:34 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -6,18 +6,8 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.124875.xyz": dockerHub,
+  "download.docker.124875.xyz": "https://download.docker.com",
 };
 
 function routeByHosts(host) {
